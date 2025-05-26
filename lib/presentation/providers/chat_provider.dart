5f746b167df81b4e448f4688a4f6de086404bc34
amgmat <alejandro.melchor@bal.com.mx>
2025-05-25 21:45:25 -0600

    provider library - show message

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messagesList = [
     Message(text: 'Hola amigo', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo', fromWho: FromWho.me),
+    Message(text: 'otro mensaje', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {}
